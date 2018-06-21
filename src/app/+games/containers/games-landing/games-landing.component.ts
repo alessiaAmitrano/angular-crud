c787fc96a1678870b2580ff1fb3f66114ad573a7
Alessia Amitrano <alessia.amitrano@holition.com>
2018-06-21 11:08:07 +0100

    demo of norm and async data

--- src/app/+games/containers/games-landing/games-landing.component.ts
+++ src/app/+games/containers/games-landing/games-landing.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GamesService } from 'core/services/games.service';
+import { Game } from 'core/models/game';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-games-landing',
   templateUrl: './games-landing.component.html',
   styleUrls: ['./games-landing.component.scss']
 })
-export class GamesLandingComponent implements OnInit {
-
+export class GamesLandingComponent implements OnInit, OnDestroy {
+  gamesList: Game[];
+  sub: Subscription;
+  gamesList$: Observable<Game[]>;
   constructor(private gameApi: GamesService) { }
 
   ngOnInit() {
-    this.gameApi.getGameList()
-      .subscribe(console.log);
+    this.sub = this.gameApi.getGameList()
+      .subscribe(data => this.gamesList = data);
+
+      this.gamesList$ = this.gameApi.getGameList();
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
