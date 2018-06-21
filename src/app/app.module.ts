d44a19b0f91396b8bc7761f9d195c3bde9a20c1f
Alessia Amitrano <alessia.amitrano@holition.com>
2018-06-21 17:48:08 +0100

    image shared component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { GamesModule } from './+games/games.module';
-import { ImageComponent } from './app/shared/components/image/image.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ImageComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   bootstrap: [AppComponent]
 })
