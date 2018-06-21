d4d7f56c3e6009dc3863fe03bf06eea2da936fb7
Alessia Amitrano <alessia.amitrano@holition.com>
2018-06-21 12:32:55 +0100

    game profile created

--- src/app/+games/games-routing.module.ts
+++ src/app/+games/games-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GamesListComponent } from './containers/games-list/games-list.component';
+import { GameProfileComponent } from '+games/containers/game-profile/game-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: GamesListComponent
+  },
+  {
+    path: ':id',
+    component: GameProfileComponent
   }
 ];
 
--- src/app/+games/games.module.ts
+++ src/app/+games/games.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GamesListComponent } from './containers/games-list/games-list.component';
 import { GamesRoutingModule } from './games-routing.module';
+import { GameProfileComponent } from './containers/game-profile/game-profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GamesRoutingModule
   ],
-  declarations: [GamesListComponent]
+  declarations: [GamesListComponent, GameProfileComponent]
 })
 export class GamesModule { }
