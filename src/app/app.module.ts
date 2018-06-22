8fce29b9152d5ff05b66cdd0b91f0cc0d4d3cd3a
Alessia Amitrano <alessia.amitrano@holition.com>
2018-06-22 10:00:25 +0100

    button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ButtonComponent } from './app/shared/components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
