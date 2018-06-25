f5ec7c8e3bb58c4f540f5626288a0eb990b6b397
alessia amitrano <alessia.amitrano@holition.com>
2018-06-25 11:40:17 +0100

    displaying add game component

--- src/app/+games/games-routing.module.ts
+++ src/app/+games/games-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GamesListComponent } from './containers/games-list/games-list.component';
 import { GameProfileComponent } from '+games/containers/game-profile/game-profile.component';
+import { AddGameComponent } from '+games/containers/add-game/add-game.component';
 
 const routes: Routes = [
   {
@@ -9,9 +10,13 @@ const routes: Routes = [
     component: GamesListComponent
   },
   {
+    path: 'add',
+    component: AddGameComponent
+  },
+  {
     path: ':id',
     component: GameProfileComponent
-  }
+  },
 ];
 
 @NgModule({
