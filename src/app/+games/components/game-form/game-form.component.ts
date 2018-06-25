060320755d4867e1290b2a9ae3f3db326f2914f1
alessia amitrano <alessia.amitrano@holition.com>
2018-06-25 14:36:13 +0100

    add game working but not as it should

--- src/app/+games/components/game-form/game-form.component.ts
+++ src/app/+games/components/game-form/game-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GamesService } from 'core/services/games.service';
 
 @Component({
   selector: 'app-game-form',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-form.component.scss']
 })
 export class GameFormComponent implements OnInit {
-
-  constructor() { }
+  gameToAdd = {
+    id: 100,
+    name: 'ziggidi',
+    year: '1921',
+    image: 'boh'
+  };
+  gameswhat: any;
+  constructor(private gameApi: GamesService) { }
 
   ngOnInit() {
+  }
+
+  addGame() {
+    this.gameswhat = this.gameApi.addGame(this.gameToAdd);
   }
 
 }
--- src/app/core/services/games.service.ts
+++ src/app/core/services/games.service.ts
@@ -1,31 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Game } from 'core/models/game';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class GamesService {
   gamesURL = 'http://localhost:3000/games';
+
   constructor(private httpC: HttpClient) { }
-  // CRUD
+  // ---CRUD--- //
 
   // READ
   getGameList(): Observable<Game[]> {
     return this.httpC.get<Game[]>(this.gamesURL);
   }
+
   // READ
   getGameById(id: number | string): Observable<Game> {
     return this.httpC.get<Game>(`${this.gamesURL}/${id}`);
   }
 
   // CREATE
-  // addGame(game: Game): Observable<Game> {
-  //   return this.httpC.post<Game>();
-  // }
+  addGame(game: Game): Observable<Game> {
+    return this.httpC.post<Game>(this.gamesURL, game, httpOptions);
+}
   // UPDATE
 
 
   // DELETE
+
+  logError(game: Game) {
+    console.log(game);
+  }
 }
