8aaca02e24653821d10935c0e913fb5178299de6
Alessia Amitrano <alessia.amitrano@holition.com>
2019-05-17 14:24:21 +0100

    Auto-generated commit

--- src/app/shared/directives/img-height.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImgHeightDirective } from './img-height.directive';
-
-describe('ImgHeightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImgHeightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
